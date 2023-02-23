5e74973ea79f627fb4db6ec01e59c5e02f76cbb9
boydjc <boyd.jc.github@gmail.com>
2023-02-23 02:16:42 +0000

    implemented simple pthread using scheduler/descheduler

--- aubatch.c
+++ aubatch.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 typedef struct Scheduler {
     char* policyName;
@@ -8,10 +11,18 @@ typedef struct Dispatcher {
 
 } Dispatcher;
 
-void schedulerCount();
-void dispatcherCount();
+void* schedulerCount();
+void* dispatcherCount();
 
 int main() {
+
+    pthread_t schedulerThread, dispatcherThread;
+
+    int iret1 = pthread_create(&schedulerThread, NULL, schedulerCount, NULL);
+    int iret2 = pthread_create(&dispatcherThread, NULL, dispatcherCount, NULL); 
+
+    pthread_join(schedulerThread, NULL);
+    pthread_join(dispatcherThread, NULL);
 
     printf("Hello from aubatch\n");
 
@@ -19,24 +30,23 @@ int main() {
 
     scheduler.policyName = "FCFS";
 
-    schedulerCount();
-    dispatcherCount();
-
     Dispatcher dispatcher;
 
     return 0;
 }
 
-void schedulerCount() {
+void* schedulerCount(void* ptr) {
     int count;
     for(count=0; count<10; count++) {
         printf("Scheduler: %i\n", count);
+        sleep(1.2);
     }
 }
 
-void dispatcherCount() {
+void* dispatcherCount(void* ptr) {
     int count;
     for(count=0; count<10; count++) {
         printf("Dispatcher: %i\n", count);
+        sleep(1);
     }
 }
