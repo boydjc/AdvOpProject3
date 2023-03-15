c07767d2cd10c6f071f84b4e6b29aca53dcf4bcc
boydjc <boyd.jc.github@gmail.com>
2023-03-15 17:44:59 +0000

    added comment blocks to source files

--- src/job.c
+++ src/job.c
@@ -1,3 +1,24 @@
+/*
+ * COMP7500/7506
+ * Project 3: AUbatch - A Batch Scheduling System
+ *  
+ * Joshua Boyd
+ * Department of Computer Science and Software Engineering
+ * Auburn University
+ * Mar. 14, 2023. Version 1.1
+ *    
+ * AUbatch is a CPU scheduling emulator. This file defines an instance of the Job structure. 
+ *    
+ * Compilation Instruction:
+ *     Compile using the 'make' command*  
+ *      
+ * How to run aubatch_sample?
+ *     1. You need to compile the batch_job file: ./src/batch_job.c
+ *     2. The "batch_job" program (see ./src/batch_job.c) takes one argument
+ *        from the commandline that is the run time.
+ *     3. In aubtach: type 'run batch_job 5 10' to submit program "batch_job" as a job.
+ */
+
 #include <job.h>
 
 
